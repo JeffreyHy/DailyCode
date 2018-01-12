ea685db1b173b1c38cebe9cf47be1063119f4138
huangyongbo <764599693@qq.com>
2018-01-12 15:59:04 +0800

    code format

--- com.huang.essay/src/test/java/com/huang/test/concurrent/ImmutableObjectTest.java
+++ com.huang.essay/src/test/java/com/huang/test/concurrent/ImmutableObjectTest.java
@@ -9,24 +9,24 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * 不可变对象测试，验证集合中对象为引用类型时，复制集合是否需要对集合中的对象进行复制
+ * 不可变对象测试，验证集合中元素为可变对象的引用时，复制集合是否需要对集合中的对象进行复制
  * Created by JeffreyHy on 2018/1/12.
  */
 public class ImmutableObjectTest extends TestCase {
 
     @Test
-    public void testImmutableObject(){
+    public void testImmutableObject() {
         Map<Integer, MissionInfo> tMap = new HashMap<>();
         for (int i = 0; i < 5; i++) {
-            tMap.put(i,new MissionInfo(i,String.valueOf(i)));
+            tMap.put(i, new MissionInfo(i, String.valueOf(i)));
         }
-        ImmutableObject  io=new ImmutableObject(tMap);
+        ImmutableObject io = new ImmutableObject(tMap);
         io.printMap();
-        Map<Integer, MissionInfo> ioMap=io.getMap();
+        Map<Integer, MissionInfo> ioMap = io.getMap();
         for (int i = 0; i < 5; i++) {
-            MissionInfo info=ioMap.get(i);
+            MissionInfo info = ioMap.get(i);
             info.setMissionId(-1);
-            ioMap.replace(i,info);
+            ioMap.replace(i, info);
         }
         io.printMap();
     }
